7f9e140c8107f2d125a9a90f5cf7c0aee1dd273d
alef-thallys <alef.thallys22@gmail.com>
2025-08-13 14:29:08 -0300

    feat: add custom OpenAPI configuration with dynamic server URL

--- src/main/java/com/github/alefthallys/desafiotecnicopetize/config/OpenApiConfig.java
+++ src/main/java/com/github/alefthallys/desafiotecnicopetize/config/OpenApiConfig.java
@@ -5,7 +5,13 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @OpenAPIDefinition(
@@ -19,4 +25,9 @@ import org.springframework.context.annotation.Configuration;
 		bearerFormat = "JWT"
 )
 public class OpenApiConfig {
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${SWAGGER_SERVER_URL:http://localhost:8080}") String serverUrl) {
+		return new OpenAPI()
+				.servers(List.of(new Server().url(serverUrl)));
+	}
 }
